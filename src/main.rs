6649536763a89f8d353f462aa83189adf91a1de0
Philipp Keller <philipp.keller@gmail.com>
2017-06-22 12:53:57 +0200

    - switch program to bytes - switch from tuple matching to match with guards

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,34 @@ use std::io::{stdout, Write};
 fn main() {
     let mut data = [0;256];
     let mut data_ptr = 0;
-    let program = ",>,[<+>-]<.".to_string();
+    let program = ",>,[<+>-]<.".as_bytes();
     let mut loop_stack = Vec::new();
 
     let mut program_ptr = 0;
     while program_ptr < program.len() {
-        match (program.chars().nth(program_ptr).unwrap(), data[data_ptr]) {
-            ('+', _) => data[data_ptr] += 1,
-            ('-', _) => data[data_ptr] -= 1,
-            ('>', _) => data_ptr += 1,
-            ('<', _) => data_ptr -= 1,
-            ('[', 0) => { // find corresponding closing bracket
+        match program[program_ptr] as char {
+            '+' => data[data_ptr] += 1,
+            '-' => data[data_ptr] -= 1,
+            '>' => data_ptr += 1,
+            '<' => data_ptr -= 1,
+            '[' if data[data_ptr] == 0 => { // find corresponding closing bracket
                 let mut open_brackets = 0;
                 program_ptr += 1;
                 loop {
-                    match (program.chars().nth(program_ptr).unwrap(), open_brackets) {
-                        ('[', _) => open_brackets += 1,
-                        (']', 0) => break,
-                        (']', _) => open_brackets -= 1,
+                    match program[program_ptr] as char {
+                        '[' => open_brackets += 1,
+                        ']' if open_brackets == 0 => break,
+                        ']' => open_brackets -= 1,
                         _ => {},
                     }
                     program_ptr += 1;
                 }
                 },
-            ('[', _) => loop_stack.push(program_ptr),
-            (']', 0) => {loop_stack.pop();},
-            (']', _) => program_ptr = *loop_stack.last().unwrap(),
-            ('.', _) => println!("{}", data[data_ptr]),
-            (',', _) => {
+            '[' => loop_stack.push(program_ptr),
+            ']' if data[data_ptr] == 0 => {loop_stack.pop();},
+            ']' => program_ptr = *loop_stack.last().unwrap(),
+            '.' => println!("{}", data[data_ptr]),
+            ',' => {
                 print!("enter number> ");
                 stdout().flush().unwrap();
                 let mut input = String::new();
